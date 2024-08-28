60a2e053f6d2ac0e559588d0603f833d1447d46f
ETHAN23\pangy <Ethant Pang>
2024-08-28 21:49:07 +0800

    rename : folder name and finish the topic of variable and memory type

--- Tutorial 2 - Addition Calculator/AdditionCalculator/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter Something");
-
-string userInput = Console.ReadLine();
-Console.WriteLine("You entered " + userInput);
-Console.ReadKey();
---
+++ Tutorial 2 - Input Output/Input Output/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter Something");
+
+string userInput = Console.ReadLine();
+Console.WriteLine("You entered " + userInput);
+
+// Reference Type
+string myName = "Denis";
+
+// defining or  setting up a variable
+string petsName;
+
+// initialize variable
+petsName = "Daisy";
+Console.WriteLine($"my ptes is {petsName}");
+
+petsName = "Barky";
+Console.WriteLine($"my ptes is {petsName}");
+
+// Value Type
+int myAge = 35;
+//double pi = 3.14;
+
+sbyte age = 57;
+short linkedinConnection = 32550;
+
+long phoneNumber = 0172312123;
+
+float pi = 3.14f;
+
+Console.ReadKey();
