580c95e24b252ba14c47b68bcb154841392fb22f
ETHAN23\pangy <Ethant Pang>
2024-08-28 11:20:44 +0800

    init : hello world console project

---
+++ Tutorial 1 - Hello World/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+// namespace - to organize where the code is located and it encapsulate the program's logic
+namespace HelloWorld {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
