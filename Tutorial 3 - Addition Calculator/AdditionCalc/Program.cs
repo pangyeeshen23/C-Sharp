855731816f7933dcee6c692623d4674e37838a59
ETHAN23\pangy <Ethant Pang>
2024-08-30 22:07:56 +0800

    learn : Culture Info and always round up floating point data type for better precision after mathtimatical operation

---
+++ Tutorial 3 - Addition Calculator/AdditionCalc/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Enter the first number");
+string firstNumber = Console.ReadLine();
+Console.WriteLine("Enter the second number");
+string secondNumber = Console.ReadLine();
+double sum = Math.Round(double.Parse(firstNumber, CultureInfo.InvariantCulture) + double.Parse(secondNumber, CultureInfo.InvariantCulture),2);
+Console.WriteLine($"The sum of {firstNumber.ToString(CultureInfo.InvariantCulture)} and {secondNumber.ToString(CultureInfo.InvariantCulture)} " +
+    $"is: {sum.ToString(CultureInfo.InvariantCulture)}");
+
