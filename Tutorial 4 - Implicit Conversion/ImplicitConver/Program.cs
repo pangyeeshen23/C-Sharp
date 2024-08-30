390debda38e7bb97a748588579ea49bb813663d2
ETHAN23\pangy <Ethant Pang>
2024-08-30 22:40:50 +0800

    learn explicit conversion and implicit conversion

---
+++ Tutorial 4 - Implicit Conversion/ImplicitConver/Program.cs
@@ -0,0 +1,20 @@
+﻿// implicit conversion
+
+int myInt = 13;
+double myDouble = myInt;
+long myLong = 132345098312389013;
+
+
+// explicit conversion (casting)
+int myInt2 = (int)myLong;
+
+double myDouble2 = 13.5;
+int myInt3 = (int)myDouble2;
+Console.WriteLine(myInt2);
+Console.WriteLine(myInt3);
+
+float myFloat = 123.123f;
+myDouble = 13.2123123123;
+
+myFloat = (float)myDouble;
+Console.WriteLine(myFloat);
