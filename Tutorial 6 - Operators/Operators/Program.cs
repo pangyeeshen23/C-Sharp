f75e5d496a6961303beca9f12324f2077eea08d8
ETHAN23\pangy <Ethant Pang>
2024-09-02 20:51:11 +0800

    operators

---
+++ Tutorial 6 - Operators/Operators/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Operators and Order of Evaluation
+int num1 = 14;
+int num2 = 13;
+
+// Concatination
+Console.WriteLine("Age is " + num1);
+
+Console.WriteLine("Addition num1 + num2 = " + num1 + num2);
+//Order of Evaluation
+Console.WriteLine("Subtraction num1 - num2 = " + (num1 - num2));
+Console.WriteLine("Subtraction num1 - num2 = " + -8);
+Console.WriteLine("Multiple num1 * num2 = " + num1 * num2);
+Console.WriteLine("Division num1 / num2 = " + num1 / num2);
