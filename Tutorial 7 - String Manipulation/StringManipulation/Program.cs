b813a4285968a32dcddc54660a9a34e5d1b04f27
ETHAN23\pangy <Ethant Pang>
2024-09-02 21:50:12 +0800

    string manipulation

---
+++ Tutorial 7 - String Manipulation/StringManipulation/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = 10;
+double price = 19.95;
+string name = "Frank";
+
+Console.WriteLine($"The number is {num}");
+Console.WriteLine("The number is " + num);
+Console.WriteLine(@"The number is ""num""
+ Number ");
+Console.WriteLine(@"C:\Users\scolderidge\Document");
+
+Console.WriteLine("The number is {0}", num);
