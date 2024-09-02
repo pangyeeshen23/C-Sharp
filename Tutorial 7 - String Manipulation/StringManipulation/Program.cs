bdf357eb73c946d509ec6b460bd08e20a174bdc4
ETHAN23\pangy <Ethant Pang>
2024-09-02 22:08:30 +0800

    string manipulator

--- Tutorial 7 - String Manipulation/StringManipulation/Program.cs
+++ Tutorial 7 - String Manipulation/StringManipulation/Program.cs
@@ -3,10 +3,15 @@ int num = 10;
 double price = 19.95;
 string name = "Frank";
 
+// Interpolation
 Console.WriteLine($"The number is {num}");
+//String Concatination
 Console.WriteLine("The number is " + num);
+
 Console.WriteLine(@"The number is ""num""
  Number ");
 Console.WriteLine(@"C:\Users\scolderidge\Document");
 
-Console.WriteLine("The number is {0}", num);
+
+//String Formating
+Console.WriteLine("The number is {0} and the price is {1}, and the name is {2}", num, price, name);
