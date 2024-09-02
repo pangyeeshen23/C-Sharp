5d4ee8b0ac0c93702a9df294c500017c7e709056
ETHAN23\pangy <Ethant Pang>
2024-09-02 22:42:00 +0800

    an example of operator overloading

---
+++ Extra/OperatorOverloading/Fraction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    public readonly struct Fraction
+    {
+
+        private readonly int num;
+        private readonly int den;
+
+        public Fraction(int numerator, int denomitor)
+        {
+            if (denomitor == 0) throw new ArgumentException("Denomitor cannot be zero.", nameof(denomitor));
+
+            num = numerator;
+            den = denomitor;
+        }
+        public static Fraction operator +(Fraction a) => a;
+        public static Fraction operator -(Fraction a) => new Fraction(-a.num, a.den);
+        public static Fraction operator +(Fraction a, Fraction b) => new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
+        public static Fraction operator -(Fraction a, Fraction b) => a + (-b);
+        public static Fraction operator *(Fraction a, Fraction b) => new Fraction(a.num * b.num, a.den * b.den);
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            if (b.num == 0) throw new DivideByZeroException();
+            return new Fraction(a.num * b.den, a.den * b.num);
+        }
+        public override string ToString() => $"{num}/{den}";
+    }
+}
---
+++ Extra/OperatorOverloading/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OperatorOverloading;
+
+Console.WriteLine("Hello, World!");
+
+var a = new Fraction(5, 4);
+var b = new Fraction(1, 2);
+Console.WriteLine(-a);
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
