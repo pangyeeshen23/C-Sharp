b2171d9e6536fd27212b4948d9842f13af8408a7
ETHAN23\pangy <Ethant Pang>
2024-09-02 20:19:09 +0800

    feat : conversion and parsing for c sharp

---
+++ Tutorial 5 - Conversion Helper/ConversionHelper/Program.cs
@@ -0,0 +1,8 @@
+﻿// Conversion Helper (Parse and Convert)
+
+string numberString = "123";
+int result = int.Parse(numberString);
+string myBoolString = "false";
+bool myBool = Convert.ToBoolean(myBoolString);
+Console.WriteLine("My bool is " + myBool);
+Console.ReadKey();
