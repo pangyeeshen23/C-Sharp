49f56b5c4bdc16b81c37dad059a9f3f64b164023
ETHAN23\pangy <Ethant Pang>
2024-09-09 20:55:13 +0800

    learn : switch statement

---
+++ SwitchState/Program.cs
@@ -0,0 +1,20 @@
+﻿int month = 5;
+string monthName = "";
+
+switch (month)
+{
+	case 1:
+		monthName = "January";
+		break;
+	case 2:
+		monthName = "February";
+		break;
+	case 3:
+		monthName = "March";
+		break;
+	default:
+		monthName = "Unknown";
+		break;
+}
+
+Console.WriteLine("The current month is " + monthName);
