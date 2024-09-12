c3bdf7758991024c0dafdab05f435b3f98951911
ETHAN23\pangy <Ethant Pang>
2024-09-12 21:32:57 +0800

    init : thread and new line in string

--- Tutorial 12 - For Looping/ForLoop/Program.cs
+++ Tutorial 12 - For Looping/ForLoop/Program.cs
@@ -1,7 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// in strings \ is an "Escape Character"
+// \n stands for "new line"
+// \r stands fr carriage return
+
+string myString = "Hi \r\nHi";
 for (int counter = 5; counter <= 10; counter++)
 {
     Console.WriteLine("Counter is " + counter);
+    Console.WriteLine(myString);
+    Thread.Sleep(1000);
 }
+
+for (int counter = 10; counter > 0; counter--)
+{
+    Console.WriteLine("Counter is " + counter);
+}
+
+Console.WriteLine("End Program");
