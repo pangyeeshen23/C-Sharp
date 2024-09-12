fa617d5adb235b5f34d17cf3ae39cf4646a38d32
ETHAN23\pangy <Ethant Pang>
2024-09-12 21:24:11 +0800

    init : for loop struct

---
+++ Tutorial 12 - For Looping/ForLoop/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int counter = 5; counter <= 10; counter++)
+{
+    Console.WriteLine("Counter is " + counter);
+}
