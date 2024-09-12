e22ce916f64b8de8fc0d1aa724e0a757391d2ac8
ETHAN23\pangy <Ethant Pang>
2024-09-12 20:26:10 +0800

    learn: pre increment, post increment. modular operator

---
+++ Tutorial 10 - PreDecrementing/PreNDecrementing/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int num = 0;
+Console.WriteLine($"Num is {num}");
+// pre-incrementing
+num++;
+//Console.WriteLine($"Num is {num}");
+//Console.WriteLine("Num is {0}", ++num);
+//Console.WriteLine("Num is {0}", num++);
+//Console.WriteLine("Num is {0}", num);
+
+// decrementing
+//num--;
+//Console.WriteLine("Num is {0}", num);
+//Console.WriteLine("Num is {0}", --num);
+//Console.WriteLine("Num is {0}", num--);
+//Console.WriteLine("Num is {0}", num);
+
+//num += 30;
+//Console.WriteLine("Num is {0}", num);
+
+//num -= 10;
+//Console.WriteLine("Num is {0}", num);
+
+//num *= 10;
+//Console.WriteLine("Num is {0}", num);
+
+//num /= 20;
+//Console.WriteLine("Num is {0}", num);
+
+//Modular Operator
+int num1 = 10;
+int num2 = 3;
+
+int result = num1 % num2;
+Console.WriteLine("Result is {0}", result);
