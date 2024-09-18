f4dd5a0d386b3a8bbd8ae8eea2eff2c905884e60
ETHAN23\pangy <Ethant Pang>
2024-09-18 14:54:18 +0800

    challenge : rocket landing with string and loop

---
+++ Extra/RocketLanding/Program.cs
@@ -0,0 +1,27 @@
+﻿string rocket = @" 
+     /\
+    /  \
+   /____\
+    |  |
+    |  |
+    |__|
+   /    \
+  /______\";
+
+string landing = "\r\n" + "\r\n" + "\r\n" + "____________";
+for (int counter = 10; counter >= 0; counter --)
+{
+    Console.Clear();
+    Console.WriteLine("Counter is " + counter);
+    rocket = "\r\n" + rocket;
+    Console.WriteLine(rocket);
+    if (counter <= 2)
+    {
+        int index = landing.IndexOf("\n");
+        landing = landing.Remove(index, 1);
+        Console.WriteLine(landing);
+    }
+    Thread.Sleep(1000);
+}
+
+Console.WriteLine("The Rocket has Landed!");
