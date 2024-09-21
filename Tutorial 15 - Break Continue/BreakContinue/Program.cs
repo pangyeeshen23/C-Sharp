1d04985b2011d116d23ea9f123f9983ebb768141
ETHAN23\pangy <Ethant Pang>
2024-09-21 18:05:12 +0800

    continue and break

---
+++ Tutorial 15 - Break Continue/BreakContinue/Program.cs
@@ -0,0 +1,11 @@
+﻿for (int i = 0; i < 10; i ++)
+{
+    Console.WriteLine(i);
+    if (i == 3)
+    {
+        //Console.WriteLine("I've had enough!");
+        //break;
+        continue;
+    }
+    Console.WriteLine("Only if not continued");
+}
