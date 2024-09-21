ba658b9b9c23e915b4ce121a3641d2893a276d40
ETHAN23\pangy <Ethant Pang>
2024-09-21 18:21:44 +0800

    learn : array declaration and print array element

---
+++ Tutorial 16 - Simple Array/SimpleArray/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] myIntArray = [5, 15, 20, 45, 55];
+string[] weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+Console.WriteLine(myIntArray[3]);
+Console.WriteLine("Today is " + weekDays[4]);
