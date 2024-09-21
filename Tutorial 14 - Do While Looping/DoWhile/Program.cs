882f8290e940720e8473583da73d0fbc9f1827fb
ETHAN23\pangy <Ethant Pang>
2024-09-21 17:51:42 +0800

    learn do while loop

---
+++ Tutorial 14 - Do While Looping/DoWhile/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int number;
+
+// do-while loop is a post-test loop
+
+do
+{
+    Console.WriteLine("Enter a positive whole number");
+    number = int.Parse(Console.ReadLine());
+} while (number <= 0);
+Console.WriteLine("Finally");
