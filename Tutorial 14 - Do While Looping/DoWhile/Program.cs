47859f6f1c50ac1f25be593bc74d0b9e4f4835c4
ETHAN23\pangy <Ethant Pang>
2024-09-21 17:57:59 +0800

    simple calculator

--- Tutorial 14 - Do While Looping/DoWhile/Program.cs
+++ Tutorial 14 - Do While Looping/DoWhile/Program.cs
@@ -1,11 +1,19 @@
 ﻿
-int number;
-
+int currentScore;
+int sum = 0;
+int counter = 0;
 // do-while loop is a post-test loop
 
 do
 {
-    Console.WriteLine("Enter a positive whole number");
-    number = int.Parse(Console.ReadLine());
-} while (number <= 0);
+    Console.WriteLine("Enter your students score. Enter -1 to finish !");
+    currentScore = int.Parse(Console.ReadLine());
+    if (currentScore == -1) continue;
+    sum = sum + currentScore;
+    counter++;
+} while (currentScore != -1);
+
+int average = sum / counter;
+Console.WriteLine("Sum is : " + sum);
+Console.WriteLine("Average is : " + average);
 Console.WriteLine("Finally");
