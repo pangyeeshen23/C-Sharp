70b89a5d07753abb3e9ff7b398580e41d98922b0
ETHAN23\pangy <Ethant Pang>
2024-09-22 20:33:53 +0800

    go thru each item in array using foreach

--- Tutorial 16 - Simple Array/SimpleArray/Program.cs
+++ Tutorial 16 - Simple Array/SimpleArray/Program.cs
@@ -1,5 +1,14 @@
 ﻿int[] myIntArray = [5, 15, 20, 45, 55];
 string[] weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-Console.WriteLine(myIntArray[3]);
-Console.WriteLine("Today is " + weekDays[4]);
+Console.WriteLine("Length of weekdays Array is: " + weekDays.Length);
+
+//for(int i = 0; i < weekDays.Length; i++)
+//{
+//    Console.WriteLine(weekDays[i]);
+//}
+
+foreach (string day in weekDays)
+{
+    Console.WriteLine(day);
+}
