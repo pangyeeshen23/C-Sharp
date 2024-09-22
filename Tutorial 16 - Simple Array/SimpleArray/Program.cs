ab70695b353ac137b6dd905f457e1d18a9fed49c
ETHAN23\pangy <Ethant Pang>
2024-09-22 21:29:23 +0800

    learn : 2 d array declaration, 3 d array declaration

--- Tutorial 16 - Simple Array/SimpleArray/Program.cs
+++ Tutorial 16 - Simple Array/SimpleArray/Program.cs
@@ -3,12 +3,59 @@ string[] weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fr
 
 Console.WriteLine("Length of weekdays Array is: " + weekDays.Length);
 
-//for(int i = 0; i < weekDays.Length; i++)
-//{
-//    Console.WriteLine(weekDays[i]);
-//}
 
 foreach (string day in weekDays)
 {
     Console.WriteLine(day);
 }
+
+int[,] array2DDeclaration = new int[3, 3];
+
+int[,,] array3DDeclaration = new int[3, 3, 3];
+
+int[,] array2DInitialized = { { 1, 2 }, { 3, 4 } };
+// [1] [2] // row 0
+// [3] [4] // row 1
+
+Console.WriteLine(array2DInitialized[0, 0]);
+
+array2DInitialized[0, 0] = 5;
+
+Console.WriteLine(array2DInitialized[0, 0]);
+
+Console.WriteLine(array2DInitialized[1, 0]);
+
+string[,] tikTacToeField = {
+    {"O","X","O" },
+    {"O","O","X" },
+    {"X","X","O" }
+};
+
+Console.WriteLine(tikTacToeField[1, 2]);
+
+string[,,] simple3DArray =
+{
+    {
+        {"000","001"},
+        {"010", "011"}
+    },
+    {
+        {"100", "101"},
+        {"110", "111"}
+    },
+    {
+        {"200", "201" },
+        {"210", "211" }
+    }
+};
+
+Console.WriteLine(simple3DArray);
+
+simple3DArray[2, 1, 0] = "Hi, whats up";
+
+Console.WriteLine(simple3DArray[0,1,0]);
+
+//for(int i = 0; i < weekDays.Length; i++)
+//{
+//    Console.WriteLine(weekDays[i]);
+//}
