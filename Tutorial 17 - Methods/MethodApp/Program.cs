78139fe65562556c2e4054a879fccdcb851672a9
ETHAN23\pangy <Ethant Pang>
2024-09-25 20:29:00 +0800

    add : method declaration

---
+++ Tutorial 17 - Methods/MethodApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// A example of a void method
+void MyFirstMethod()
+{
+    Console.WriteLine("My First Method Was Called");
+    Console.WriteLine("Some super complicated code");
+}
+MyFirstMethod();
+
+//Method Declaration
+void WriteSomethingStupid(string myString)
+{
+    Console.WriteLine("You pass this argument to me " + myString);
+}
+
+string username = "Frank";
+//Calling the method using an Argument called username
+WriteSomethingStupid(username);
+
+Console.WriteLine("This is outside of the method");
+Console.ReadKey();
