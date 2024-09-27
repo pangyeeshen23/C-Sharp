68d65e28f9b53021c24aee2b968a508841d5a8f4
ETHAN23\pangy <Ethant Pang>
2024-09-27 16:21:30 +0800

    method, scope, weather generator

---
+++ App/WeatherStationSimulator/WeatherStationSimulator/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace WeatherStationSimulator
+{
+    internal class Program
+    {
+        private static int[] _temperatures;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of days to simulate:");
+            int days = int.Parse(Console.ReadLine());
+            string[] weatherConditions = new string[days];
+            weatherConditions = GenerateWeatherConditions(days);
+            Console.WriteLine($"Average Temp is : {CalculateTempAverage()}");
+        }
+
+        static string[] GenerateWeatherConditions(int days)
+        {
+            Random random = new Random();
+            _temperatures = new int[days];
+            string[] result = new string[days];
+            string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
+            for (int i = 0; i < days; i++) {
+                _temperatures[i] = random.Next(-10, 40);
+                result[i] = conditions[random.Next(conditions.Length)];
+            }
+            return result;
+        }
+
+        static decimal CalculateTempAverage() {
+            decimal sum = 0;
+            for (int i = 0; i < _temperatures.Length; i++)
+            {
+                sum += _temperatures[i];
+            }
+            return sum / _temperatures.Length;
+        }
+    }
+}
---
+++ Tutorial 17 - Methods/OldStyleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace OldStyleApp
+{
+    internal class Program
+    {
+        // Field  (or instance variable - sometimes even called global variables)
+        private static int myResult;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number, I'll add 10 to it!");
+            int num1 = int.Parse(Console.ReadLine());
+            myResult = AddTwoValues(num1, 10);
+            Console.WriteLine("The Result is " + myResult);
+
+            ChildProgram childProgram = new ChildProgram();
+            childProgram.Process();
+        }
+
+        public static void ShowMessage()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        static int AddTwoValues(int value1, int value2)
+        {
+            myResult = value1 + value2;
+            return myResult;
+        }
+
+        static int SubtractTwoValues(int value1, int value2)
+        {
+            myResult = (value1 - value2);
+            return myResult;
+        }
+    }
+
+    public class ChildProgram
+    {
+        public void Process()
+        {
+            Program.ShowMessage();
+        }
+    }
+}
