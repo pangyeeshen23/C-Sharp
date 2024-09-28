3262cee38da8dfbff82c81cf43d4365e1c2f2194
ETHAN23\pangy <Ethant Pang>
2024-09-28 17:26:49 +0800

    learn : properties, field, encapsulation, constructor, override

---
+++ Tutorial 18 - Classes/ClassesApp/Car.cs
@@ -0,0 +1,49 @@
+﻿namespace ClassesApp
+{
+    /// <summary>
+    /// It's internal, which means that
+    /// it can only be accessed from
+    /// within the same assembly
+    /// </summary>
+    internal class Car
+    {
+
+        // member variable / field
+        // private keywords
+        private string _model = "";
+        private string _brand = "";
+        private bool _isLuxury = false;
+
+        // properties
+        public string Model { get => _model; set => _model = value; }
+        public string Brand { 
+            get
+            {
+                if (_isLuxury)
+                {
+                    return _brand + " - Luxury Edition";
+                }
+                else
+                {
+                    return _brand;
+                }
+            }
+
+            set
+            {
+                _brand = value;
+            }
+        }
+        public bool IsLuxury { get => _isLuxury; set => _isLuxury = value; }
+
+        // Constructor
+        public Car(string model, string brand, bool isLuxury)
+        {
+            Model = model;
+            Brand = brand;
+            IsLuxury = isLuxury;
+            Console.WriteLine($"A {_brand} of the model {_model} has been created");
+        }
+
+    }
+}
---
+++ Tutorial 18 - Classes/ClassesApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an object of the class Car
+            // Creating an instance of the class Car
+            Car audi = new Car("A3", "Audi", false);
+            Car bmw = new Car("i7", "BMW", true);
+            Console.WriteLine("Please enter the Brand name");
+            Console.WriteLine("Brand is " + audi.Brand);
+            Console.WriteLine("Brand is " + bmw.Brand);
+        }
+    }
+}
