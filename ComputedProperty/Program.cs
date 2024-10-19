2adbdf2e5141923bcbbfe5b66c4acce6439dd4ea
ETHAN23\pangy <Ethant Pang>
2024-10-19 17:38:59 +0800

    learn : Computed Property

---
+++ ComputedProperty/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ComputedProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            r1.Width = 5;
+            r1.Height = 5;
+            Console.WriteLine("Area of r1 is " + r1.Area);
+        }
+    }
+}
