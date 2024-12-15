fb392da80e8ef7953db52ed0f0c2f41a9843ecb1
ETHAN23\pangy <Ethant Pang>
2024-12-15 16:41:44 +0800

    learn : nullable types

---
+++ Tutorial 23 - Nullable/NullableList/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NullableList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? age = null; // int? is a nullable int
+            int myAge = 35;
+
+            if (age.HasValue)
+            {
+                int sum = age.Value + myAge;
+                Console.WriteLine("Age is:" + age.Value);
+            }
+            else
+            {
+                Console.WriteLine("Age is not specified");
+            }
+        }
+    }
+}
