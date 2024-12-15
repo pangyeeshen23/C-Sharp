dcf60d22a8a16280d0e4b1e384c7cb3d5ae1309c
ETHAN23\pangy <Ethant Pang>
2024-12-15 16:01:31 +0800

    learn : array list

---
+++ Tutorial 22 - Legacy List/LegacyList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+namespace LegacyList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList(); // undefined amount of object
+            ArrayList arrayList2 = new ArrayList(100); // defined amount of object
+
+            arrayList.Add(12);
+            arrayList.Add("Name");
+            arrayList.Add(13.37);
+            arrayList.Add(128);
+            arrayList.Add('C');
+            arrayList.Add("Name");
+
+            // delete element with specific entry from the array list
+            arrayList.Remove("Name");
+
+            // delete element at specific position
+            arrayList.RemoveAt(0);
+
+            int count = arrayList.Count;
+
+            Console.WriteLine(count);
+            double sum = 0;
+            foreach (object obj in arrayList)
+            {
+                if (obj is int)
+                {
+                    sum += Convert.ToDouble(obj);
+                }
+                else if (obj is double)
+                {
+                    sum += (double) obj;
+                }
+                else if (obj is string)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
