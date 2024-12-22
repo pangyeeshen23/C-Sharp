972815cfb891a5dac466ba10207f31942d7deef8
ETHAN23\pangy <Ethant Pang>
2024-12-22 10:23:04 +0800

    learn : dictionary

---
+++ Tutorial 24 - Dictionary/Dictionaries/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // key - value
+            // declaring and initializing a Dictionary
+            Dictionary<int, string> employees = new Dictionary<int, string>();
+
+            // Adding Items to a Dictionary
+            employees.Add(101, "John Doe");
+            employees.Add(102, "Bob Smith");
+            employees.Add(103, "Rob Smith");
+            employees.Add(104, "Flob Smith");
+            employees.Add(105, "Dob Smith");
+
+            string name = employees[101];
+            Console.WriteLine(name);
+
+            // access item in a dictionary
+            employees[102] = "Jane Smith";
+
+            // remove an item form a dictionary
+            employees.Remove(101);
+
+            foreach(KeyValuePair<int, string> employee in employees)
+            {
+                Console.WriteLine($"Id: {employee.Key}, Name: {employee.Value}");
+            }
+
+        }
+    }
+}
