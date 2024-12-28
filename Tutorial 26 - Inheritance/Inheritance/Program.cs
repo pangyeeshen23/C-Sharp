4bbd801c4c3638aa0c24cdc517fa223861b15d31
ETHAN23\pangy <Ethant Pang>
2024-12-28 17:13:42 +0800

    learn : inheritance

---
+++ Tutorial 26 - Inheritance/Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Eat();
+            myDog.Bark();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+    // Base Class (Parent Class or Superclass)
+    class Animal
+    {
+        public void Eat()
+        { 
+        
+            Console.WriteLine("Eating ...");
+        }
+    }
+
+    //Derived Class (Child Class or Subclass):
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking ...");
+        }
+    }
+}
