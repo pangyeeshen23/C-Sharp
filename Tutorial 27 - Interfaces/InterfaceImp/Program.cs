70a3166d56ffbcb82c85e0576ba505374e6d1527
ETHAN23\pangy <Ethant Pang>
2025-01-19 14:44:07 +0800

    learn : interface

---
+++ Tutorial 27 - Interfaces/InterfaceImp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace InterfaceImp
+{
+    public interface IAnimal
+    {
+        void MakeSound();
+        void Eat(string food);
+    }
+
+    public class Dog : IAnimal
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine("Dog ate " + food);
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine("Cat ate " + food);
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Dog dog = new Dog();
+            dog.MakeSound();
+            dog.Eat("Treat");
+        }
+    }
+}
