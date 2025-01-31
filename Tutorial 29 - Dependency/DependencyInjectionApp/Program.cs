63301ddbe8ae3f8a838231d767266a0cc8dbc0ae
ETHAN23\pangy <Ethant Pang>
2025-01-31 15:33:51 +0800

    learn : inteface di

--- Tutorial 29 - Dependency/DependencyInjectionApp/Program.cs
+++ Tutorial 29 - Dependency/DependencyInjectionApp/Program.cs
@@ -16,26 +16,49 @@
         }
     }
 
-    public class Builder
+    public interface IToolUser
+    {
+        void SetHammer(Hammer hammer);
+        void SetSaw(Saw saw);
+    }
+
+    public class Builder : IToolUser
     {
         public Hammer Hammer { get; set; }
         public Saw Saw { get; set; }
 
-        //private Hammer _hammer;
-        //private Saw _saw;
+        private Hammer _hammer;
+        private Saw _saw;
 
-        // Constructur Dependency Injector (DI)
-        //public Builder(Hammer hammer, Saw saw)
-        //{
-        //    _hammer = hammer;
-        //    _saw = saw; 
-        //}
+        public Builder()
+        {
+            
+        }
+
+        //Constructur Dependency Injector(DI)
+        public Builder(Hammer hammer, Saw saw)
+        {
+            _hammer = hammer;
+            _saw = saw;
+        }
 
         public void BuildHouse()
         {
-            Hammer.Use();
-            Saw.Use();
+            Hammer hammer = (Hammer != null) ? Hammer : _hammer;
+            Saw saw = (Saw != null) ? Saw : _saw; ;
+            _hammer.Use();
+            _saw.Use();
             Console.WriteLine("House build");
+        }
+
+        public void SetHammer(Hammer hammer)
+        {
+            _hammer = hammer;
+        }
+
+        public void SetSaw(Saw saw)
+        {
+            _saw = saw;
         }
     }
 
@@ -48,9 +71,14 @@
             //Saw saw = new Saw();
             //Builder builder = new Builder(hammer, saw);
 
+            //Builder builder = new Builder();
+            //builder.Hammer = new Hammer(); // Inject dependencies via Setters
+            //builder.Saw = new Saw(); // Inject dependencies via Setters
+
+
             Builder builder = new Builder();
-            builder.Hammer = new Hammer(); // Inject dependencies via Setters
-            builder.Saw = new Saw(); // Inject dependencies via Setters
+            builder.SetHammer(new Hammer());
+            builder.SetSaw(new Saw());
             builder.BuildHouse();
             Console.WriteLine("Hello, World!");
         }
