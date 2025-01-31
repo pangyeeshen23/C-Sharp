2c56520815803eb7bc795f1b8bc6b64c1a6e1fc7
ETHAN23\pangy <Ethant Pang>
2025-01-31 14:54:52 +0800

    learn : File and decoupling with interfaces

---
+++ Tutorial 28 - File/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "This is a log entry";
+            ILogger fileLogger = new FileLogger();
+            fileLogger.Log(message);
+            Application application = new Application(fileLogger);
+            application.DoWork(message);
+
+            ILogger databaseLogger = new DatabaseLogger();
+            Application dbApplication = new Application(databaseLogger);
+            dbApplication.DoWork(message);
+        }
+    }
+
+    public class Application
+    {
+        private readonly ILogger _logger;
+        public Application(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoWork(string message)
+        {
+            _logger.Log("Work Started");
+            // Do All The Work!
+            _logger.Log("Work Done");
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class DatabaseLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"Logging to database. {message}");
+        }
+    }
+
+    public class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            string directoryPath = @"C:\Logs";
+            string filePath = Path.Combine(directoryPath, "log.txt");
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+            File.AppendAllText(filePath, message + "\n");
+        }
+    }
+}
