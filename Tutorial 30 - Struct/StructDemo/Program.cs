03dafa729801add917fbc42c29c9ee001138b3a9
ETHAN23\pangy <Ethant Pang>
2025-02-01 14:38:19 +0800

    learn : constructor

---
+++ Tutorial 30 - Struct/StructDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace StructDemo
+{
+
+    public struct Point
+    {
+        public int X;
+        public int Y;
+        //public int X { get; set; }
+        //public int Y { get; set; }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Point: ({X}, {Y})");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point(10 , 20);
+            point.Display();
+
+            Point point2;
+            point2.X = 10;
+            point2.Y = 20;
+            point2.Display();
+        }
+    }
+}
