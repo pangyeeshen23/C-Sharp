da85f79e796cb663f4d12737ef3b7d88722f47ee
ETHAN23\pangy <Ethant Pang>
2025-02-16 14:50:20 +0800

    learn : event, dynmically insert button

--- Tutorial 35 - WPF/Tutorial 1 - Demo/Demo/Challenge.xaml.cs
+++ Tutorial 35 - WPF/Tutorial 1 - Demo/Demo/Challenge.xaml.cs
@@ -23,5 +23,10 @@ namespace Demo
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
