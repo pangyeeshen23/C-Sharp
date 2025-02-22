fd7732719c89eb4eaf63bbf116aa7c6d218cd0af
ETHAN23\pangy <Ethant Pang>
2025-02-22 16:10:01 +0800

    learn : one way, two way, one way to souce and one time data binding

---
+++ Tutorial 35 - WPF/Tutorial 4 - Data Binding/DataBindingApp/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DataBindingApp.Data;
+using System.Windows;
+
+namespace DataBindingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person()
+        {
+            Name = "Ethan",
+            Age = 23
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string personData = person.Name + " is " + person.Age + " years old";
+            MessageBox.Show(personData);
+        }
+    }
+}
