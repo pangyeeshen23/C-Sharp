af05d736ef0a735bf7506a4e59d717f21d49125a
ETHAN23\pangy <Ethant Pang>
2025-02-22 16:20:21 +0800

    learn : list item

---
+++ Tutorial 35 - WPF/Tutorial 4 - Data Binding/DataBindingApp/ListWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DataBindingApp.Data;
+using System.Windows;
+
+namespace DataBindingApp
+{
+    /// <summary>
+    /// Interaction logic for ListWindow.xaml
+    /// </summary>
+    public partial class ListWindow : Window
+    {
+        private List<Person> People = new List<Person>()
+        {
+            new Person()
+            {
+                Name = "Ethan",
+                Age = 25
+            },
+            new Person()
+            {
+                Name = "Daniel",
+                Age = 40
+            },
+            new Person()
+            {
+                Name = "Jonny",
+                Age = 35
+            }
+        };
+
+        public ListWindow()
+        {
+            InitializeComponent();
+            ListBoxPeople.ItemsSource = People;
+        }
+    }
+}
