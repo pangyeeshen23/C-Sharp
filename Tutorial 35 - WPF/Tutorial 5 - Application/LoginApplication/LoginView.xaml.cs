8b31f5f281c0397391aff1fac1cf59617c807460
ETHAN23\pangy <Ethant Pang>
2025-03-02 14:16:34 +0800

    complete : login app

--- Tutorial 35 - WPF/Tutorial 5 - Application/LoginApplication/LoginView.xaml.cs
+++ Tutorial 35 - WPF/Tutorial 5 - Application/LoginApplication/LoginView.xaml.cs
@@ -24,5 +24,38 @@ namespace LoginApplication
         {
             InitializeComponent();
         }
+
+        private void OnLoginButtonClicked(object sender, RoutedEventArgs e)
+        {
+            string password = LoginPasswordBox.Password;
+            string? envPw = Environment.GetEnvironmentVariable("InvoiceManagement");
+            if (envPw != null)
+            {
+                if (password == envPw)
+                {
+                    MessageBox.Show("Entered correct Password");
+                }
+                else
+                {
+                    MessageBox.Show("Entered wrong password");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Environment variable not found");
+            }
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(LoginPasswordBox.Password))
+            {
+                LoginButton.IsEnabled = true;
+            }
+            else
+            {
+                LoginButton.IsEnabled = false;
+            }
+        }
     }
 }
