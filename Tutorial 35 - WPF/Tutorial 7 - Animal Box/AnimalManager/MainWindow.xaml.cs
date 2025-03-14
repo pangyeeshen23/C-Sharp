e294383dc21f37635ae69f7195fae2d5ca40fbf5
ETHAN23\pangy <Ethant Pang>
2025-03-14 21:27:08 +0800

    init : animal manager with data source

---
+++ Tutorial 35 - WPF/Tutorial 7 - Animal Box/AnimalManager/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Windows;
+
+namespace AnimalManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["AnimalManager.Properties.Settings.animalsConnectionString"].ConnectionString;
+        }
+    }
+}
