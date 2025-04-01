52be10426567a5a00f714b0b754712e7b5a57909
Ethan <pangyeeshen2@gmail.com>
2025-04-01 14:12:51 +0800

    feat : odd numbers

---
+++ Tutorial 36 - Linq/LinqOne/Program.cs
@@ -0,0 +1,16 @@
+﻿int[]number = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+OddNumbers(number);
+Console.ReadKey();
+
+static void OddNumbers(int[] numbers)
+{
+    Console.WriteLine("Odd Numbers: ");
+    IEnumerable<int> oddNumbers = from number in numbers
+                                  where number % 2 != 0
+                                  select number;
+    Console.WriteLine(oddNumbers);
+    foreach(int number in oddNumbers)
+    {
+        Console.WriteLine(number);
+    }
+}
