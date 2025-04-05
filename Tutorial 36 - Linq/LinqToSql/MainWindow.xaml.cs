10fe073e0a91afd0dc30e846474bfb9196a2ec96
Ethan <pangyeeshen2@gmail.com>
2025-04-05 16:19:30 +0800

    setup : db connection

--- Tutorial 36 - Linq/LinqToSql/MainWindow.xaml.cs
+++ Tutorial 36 - Linq/LinqToSql/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LinqToSql
 {
@@ -20,9 +8,12 @@ namespace LinqToSql
     /// </summary>
     public partial class MainWindow : Window
     {
+        LinqToSqlDataClassesDataContext dataContext;
         public MainWindow()
         {
-            //universityConnectionString
+            string connectionString = ConfigurationManager
+                .ConnectionStrings["LinqToSql.Properties.Settings.universityConnectionString"].ConnectionString;
+            dataContext = new LinqToSqlDataClassesDataContext(connectionString);
             InitializeComponent();
         }
     }
