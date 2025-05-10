eb73457f67841ce66d4a2cad9bcef0762ad081c1
Ethan <pangyeeshen2@gmail.com>
2025-05-10 17:15:51 +0800

    learn : test trustwhortiness, devils advocate and parameterized test

---
+++ Tutorial 40 - Testing Framework/Flight/Domain/Flight.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain
+{
+    public class Flight
+    {
+        public int RemainingNumberOfSeats { get; set; }
+
+        public Flight(int seatCapacity)
+        {
+            RemainingNumberOfSeats = seatCapacity;
+        }
+
+        public void Book(string email, int numberOfSeats)
+        {
+            if (numberOfSeats > RemainingNumberOfSeats)
+            {
+                throw new OverbookingException();
+            }
+
+            RemainingNumberOfSeats -= numberOfSeats;
+        }
+    }
+}
---
+++ Tutorial 40 - Testing Framework/Flight/FlightSpecification.cs
@@ -0,0 +1,53 @@
+using Domain;
+using FluentAssertions;
+
+namespace Test
+{
+    public class FlightSpecifications
+    {
+        [Fact]
+        public void Avoids_overbooking()
+        {
+            var flight = new Flight(seatCapacity: 3);
+            Assert.Throws<OverbookingException>
+                (() => flight.Book("yeeshen2@gmail.com", 4)
+           );
+        }
+
+        [Fact]
+        public void Booking_reduces_the_number_of_seats_by_one()
+        {
+            var flight = new Flight(seatCapacity: 3);
+            flight.Book("yeess@gmail.com", 1);
+            flight.RemainingNumberOfSeats.Should().Be(2);
+        }
+
+
+        [Fact]
+        public void Booking_reduces_the_number_of_seats_by_three()
+        {
+            var flight = new Flight(seatCapacity: 6);
+            flight.Book("yeess@gmail.com", 3);
+            flight.RemainingNumberOfSeats.Should().Be(3);
+        }
+
+        [Fact]
+        public void Booking_reduces_the_number_of_seats_by_six()
+        {
+            var flight = new Flight(seatCapacity: 10);
+            flight.Book("yeess@gmail.com", 6);
+            flight.RemainingNumberOfSeats.Should().Be(4);
+        }
+
+        [Theory]
+        [InlineData(3, 1, 2)]
+        [InlineData(6, 3, 3)]
+        [InlineData(10, 6, 4)]
+        public void Booking_reduces_the_number_of_seats_by_input(int seatCapacity, int numberOfSeats, int remainingNumberOfSeats)
+        {
+            var flight = new Flight(seatCapacity: seatCapacity);
+            flight.Book("yeess@gmail.com", numberOfSeats);
+            flight.RemainingNumberOfSeats.Should().Be(remainingNumberOfSeats);
+        }
+    }
+}
