302a86f906d7bd22ca5d12c02c29be7cfda63608
Ethan <pangyeeshen2@gmail.com>
2025-06-21 22:41:57 +0800

    feat : add in game model

--- Tutorial 41 - Unity/Fruit Ninja/Assets/Scritps/Blade.cs
+++ Tutorial 41 - Unity/Fruit Ninja/Assets/Scritps/Blade.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+    public float minVelo = 0.1f;
     private Rigidbody2D rb;
+    private Vector3 lastMousePos;
+    private Vector3 mouseVelo;
+    private Collider2D col;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     private void Update()
     {
+        col.enabled = IsMouseMoving();
         SetBladeToMouse();
     }
 
@@ -18,7 +24,16 @@ public class Blade : MonoBehaviour
     {
         var mousePos = Input.mousePosition;
         mousePos.z = 10;
-        
         rb.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private bool IsMouseMoving()
+    {
+        Vector3 curMousePos = transform.position;
+        float traveled = (lastMousePos - curMousePos).magnitude;
+        lastMousePos = curMousePos;
+
+        if (traveled > minVelo) return true;
+        else return false;
     }
 }
