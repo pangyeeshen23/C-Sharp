9fba025ce704da640d62b4a9efc622b51cc1e07a
Ethan <pangyeeshen2@gmail.com>
2025-06-21 14:45:35 +0800

    feat : add in bomb, highscore, score, game over panel, restart game

--- Tutorial 41 - Unity/Fruit Ninja/Assets/Scritps/GameManager.cs
+++ Tutorial 41 - Unity/Fruit Ninja/Assets/Scritps/GameManager.cs
@@ -11,26 +11,50 @@ public class GameManager : MonoBehaviour
     [Header("GameOver")]
     public GameObject gameOverPanel;
     public TextMeshProUGUI gameOverPanelScoreText;
+    public TextMeshProUGUI gameOverPanelHighScoreText;
+
+    private int highScore;
 
     private void Awake()
     {
         gameOverPanel.SetActive(false);
+        highScore = PlayerPrefs.GetInt("HighScore");
+        highScoreText.text = "Best: " + highScore.ToString();
     }
 
     public void IncreaseScore(int points)
     {
         score += points;
-        int highScore = PlayerPrefs.GetInt("HighScore", 0);
-        if (score > highScore) PlayerPrefs.SetInt("HighScore", score);
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreText.text = "Best: " + score.ToString();
+        }
         scoreText.text = score.ToString();
-        highScoreText.text = "Best: " + score.ToString();
     }
 
     public void OnBombHit()
     {
         Time.timeScale = 0;
+        scoreText.text = "0";
+        highScoreText.text = "Best: 0";
         gameOverPanelScoreText.text = "Score: " + score.ToString();
+        highScore = PlayerPrefs.GetInt("HighScore");
+        gameOverPanelHighScoreText.text = "High Score: " + highScore.ToString();
         gameOverPanel.SetActive(true);
-        Debug.Log("Bomb Hit");
+    }
+
+    public void RestartGame()
+    {
+        score = 0;
+        scoreText.text = "0";
+        gameOverPanel.SetActive(false);
+        gameOverPanelScoreText.text = "Score: 0";
+        highScoreText.text = "Best: " + highScore.ToString();
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Interactable"))
+        {
+            Destroy(g);
+        }
+        Time.timeScale = 1;
     }
 }
