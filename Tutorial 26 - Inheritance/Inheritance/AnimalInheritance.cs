5c6b6ce7460753386e9aea204ac84c59047823ee
Ethan <pangyeeshen2@gmail.com>
2025-06-22 20:54:57 +0800

    learn : sealed and new keyword.

--- Tutorial 26 - Inheritance/Inheritance/AnimalInheritance.cs
+++ Tutorial 26 - Inheritance/Inheritance/AnimalInheritance.cs
@@ -23,6 +23,9 @@ namespace Inheritance
             dog.MakeSound();
             Console.WriteLine(myDog);
 
+            Animal dog2 = new Dog();
+            dog2.Sit();
+
         }
 
         // Base Class (Parent Class or Superclass)
@@ -38,16 +41,26 @@ namespace Inheritance
             {
                 Console.WriteLine("Animal makes a generic sound");
             }
+
+            public void Sit()
+            {
+                Console.WriteLine("Animal Sit");
+            }
         }
 
         //Derived Class (Child Class or Subclass):
         // the class that inherits the members of the base class
         class Dog : Animal
         {
-            public override void MakeSound()
+            public override sealed void MakeSound()
             {
                 base.MakeSound();
                 Console.WriteLine("Barking ...");
+            }
+
+            public new void Sit()
+            {
+                Console.WriteLine("Dog Sit");
             }
         }
 
@@ -58,12 +71,23 @@ namespace Inheritance
             {
                 Console.WriteLine("Cat is meowing");
             }
+
+            public new void Sit()
+            {
+                Console.WriteLine("Cat Sit");
+            }
         }
 
         // a breed of dog
         // This is multi-level inheritance
         class Collie : Dog
         {
+            // this will result in compiler error because MakeSound is sealed in Dog class
+            //public override void MakeSound()
+            //{
+
+            //}
+
             public void GoingNuts()
             {
                 Console.WriteLine("Collie going nuts");
